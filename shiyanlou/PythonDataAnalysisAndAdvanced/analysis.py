20cf8b636531e6b8412d944c78424e1b77da307b
sunlu <sunlu900326@yeah.net>
2017-10-09 17:08:38 +0800

    analysis

---
+++ shiyanlou/PythonDataAnalysisAndAdvanced/analysis.py
@@ -0,0 +1,18 @@
+# 需要使用 json 包解析 json 文件
+import json
+import numpy
+import pandas
+
+
+def analysis(file, user_id):
+    times = 0
+    minutes = 0
+
+    # 完成剩余代码
+    f = open(file)
+    jsonFile = json.load(f)
+    # if jsonFile['user_id'] == user_id:
+
+
+
+    return times, minutes
